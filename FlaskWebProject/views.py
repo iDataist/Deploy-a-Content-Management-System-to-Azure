c6c760f24a983c0012ddf32c9bdb9218faad3b3c
Hui Ren <huiren16@gmail.com>
2021-04-12 14:30:41 -0500

    Revert "Update .DS_Store"
    This reverts commit bc0234bde12ce4ed826f1d985f4a4a66922c767c.

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,11 +66,9 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.info('Invalid login attempt')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        app.logger.info('Logged in successfully')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
